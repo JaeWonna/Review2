3e19f531dc178ae88ff8124ed422f96d8b6c30a4
JaeWonna <highjae09@gmail.com>
2023-11-20 19:03:45 +0900

    JWT, OAUTH2로 로그인/로그아웃 구현하기

--- src/main/java/me/jaewonna2/springbootdeveloper/domain/User.java
+++ src/main/java/me/jaewonna2/springbootdeveloper/domain/User.java
@@ -29,50 +29,56 @@ public class User implements UserDetails {
     @Column(name = "password")
     private String password;
 
+    @Column(name = "nickname", unique = true)
+    private String nickname;
+
     @Builder
-    public User(String email, String password, String auth){
+    public User(String email, String password, String nickname) {
         this.email = email;
         this.password = password;
+        this.nickname = nickname;
     }
 
-    @Override // 권한 반환
-    public Collection<? extends GrantedAuthority> getAuthorities(){
+    public User update(String nickname) {
+        this.nickname = nickname;
+
+        return this;
+    }
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of(new SimpleGrantedAuthority("user"));
     }
 
-    // 사용자의 id를 반환(고유한 값)
     @Override
-    public String getUsername(){
+    public String getUsername() {
         return email;
     }
 
-    // 사용자의 패스워드를 반환
     @Override
-    public String getPassword(){
+    public String getPassword() {
         return password;
     }
-    
-    // 계정 만료 여부 반환
+
     @Override
-    public boolean isAccountNonExpired(){
-        return true; // true -> 만료되지 않았음
+    public boolean isAccountNonExpired() {
+        return true;
     }
 
-    // 계정 잠금 여부 반환
     @Override
-    public boolean isAccountNonLocked(){
-        return true; // true -> 잠금되지 않았음
+    public boolean isAccountNonLocked() {
+        return true;
     }
 
-    // 패스워드 만료 여부 반환
     @Override
-    public boolean isCredentialsNonExpired(){
-        return true; // true -> 만료되지 않았음
+    public boolean isCredentialsNonExpired() {
+        return true;
     }
 
-    // 계정 사용 가능 여부 반환
     @Override
-    public boolean isEnabled(){
+    public boolean isEnabled() {
         return true;
     }
 }
